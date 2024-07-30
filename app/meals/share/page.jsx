82074b0ee506cdb0243c32913ea7ea8123e34f52
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-30 14:18:48 -0300

    Adding the form logic with server actions

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,6 +1,21 @@
+import ImagePicker from "@/components/meals/ImagePicker/ImagePicker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      summary: formData.get("summary"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
   return (
     <>
       <header className={classes.header}>
@@ -10,7 +25,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,7 +53,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
