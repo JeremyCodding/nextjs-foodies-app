15f5f02d382b47dee9ba33fea6c207970e45a76f
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-30 10:04:00 -0300

    Doing meals page

---
+++ components/meals/MealsGrid/meals-grid.jsx
@@ -0,0 +1,14 @@
+import MealItem from "../MealItem/MealItem";
+import styles from "./meals-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meals} />
+        </li>
+      ))}
+    </ul>
+  );
+}
