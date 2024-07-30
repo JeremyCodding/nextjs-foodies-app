d4072b0e8f70b3fbf53e8e0591ec28ea86731846
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-30 10:30:34 -0300

    Adding DB and fixing meals page

--- components/meals/MealsGrid/MealsGrid.jsx
+++ components/meals/MealsGrid/MealsGrid.jsx
@@ -1,12 +1,13 @@
 import MealItem from "../MealItem/MealItem";
-import styles from "./meals-grid.module.css";
+
+import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul className={styles.meals}>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
-          <MealItem {...meals} />
+          <MealItem {...meal} />
         </li>
       ))}
     </ul>
